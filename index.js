eadcca7787e828959de178c330ff8e030cc6fb24
pokornajohana <pokorna.johana1@gmail.com>
2022-02-23 21:56:44 +0100

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// Import stylesheets
-import './style.css';
+import './training/traning.js';
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
---
+++ training/traning.js
@@ -0,0 +1,39 @@
+/*console.log('ahoj');
+
+alert('pepa');
+
+prompt('kolik mas let');*/
+
+/*let jmeno = 'Johana';
+let vek = 26;
+let svobodna = true;
+
+console.log('Jmenuji se ' + jmeno + ' a je mi ' + vek + '.');*/
+
+/*let vek = 2020 - 1990;
+
+console.log('je mi' + vek);*/
+
+//vypocti pocet minut v celem lednu
+
+/*let dnuVlednu = 31;
+let hodinDen = 24;
+let minutHodina = 60;
+
+console.log('Leden má dohromady ' + dnuVlednu * hodinDen * minutHodina);*/
+
+/*let jmeno = 'Petra';
+let vek = 34;
+let vaha = 65;
+let vyska = 1.67;
+
+console.log(
+  'Jmenuji se ' +
+    jmeno +
+    ', je mi ' +
+    vek +
+    ', vazim ' +
+    vaha +
+    ' a moje BMI je ' +
+    vaha / (vyska * vyska)
+);*/
